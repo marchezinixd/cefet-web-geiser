fd3cc0320b27384ae8dcd9f5c67bc5aea8754f5f
Aluno <aluno@decom-OptiPlex-780>
2016-05-12 12:11:47 -0300

    Falta Ex 3

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 var express = require('express'),
     app = express();
-
+var fs = require('fs');
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
 var db = {
+  jogador : JSON.parse(fs.readFileSync('server/data/jogadores.json')).players,
+  jogosPorJogador: JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json'))
 };
 
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
+app.set('view engine', 'hbs');
+app.set('views', 'server/views');
 
 
 // EXERCÍCIO 2
@@ -17,17 +20,27 @@ var db = {
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json
+app.get('/', function(request, response) {
+  response.render('index', {jogador:db.jogador});
+});
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter umas 15 linhas de código
+app.get('/jogador/:id/',function(req, res){
+  response.render('jogador.hbs', {});
 
+});
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
+app.use(express.static('client'));
 
 // abrir servidor na porta 3000
 // dica: 1-3 linhas de código
+var server = app.listen(3000, function () {
+  console.log('Escutando em: http://localhost:3000');
+});
